82404364342e108ab261825395bbbcc69e14015a
katsa9 <kathleen.brand2@gmail.com>
2019-08-13 19:27:05 +0200

    dev: completed leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,39 +5,57 @@ import { leaderboardData } from '../utils/helpers'
 class Leaderboard extends Component {
   render () {
     return (
-      <div className="green-container">
-        {this.props.users.map((user) => (
-          <div className="container">
-          <b>{user.name}</b>
-          <hr></hr>
-          <div className="container">
-            {/* <div className="row"> */}
-              <div className="avatar-container col-sm-3 col-md-3">
-                <img
-                  src={user.avatar}
-                  alt={`Avatar of ${user.name}`}
-                  className="avatar"
-                  width="120"
-                  height="120"
-                />
-              </div>
-              <div className="col-sm-9 col-md-9">
-                <p>Questions Answered: {user.answered}</p>
+      <div className="green-container px-4">
+        <ul>
+          {this.props.users.map((user) => (
+            <li key={user.id}>
+              <div className="container">
+                <b>{user.name}</b>
                 <hr></hr>
-                <p>Questions Created: {user.created}</p>
-              {/* </div> */}
-            </div>
-          </div>
-        </div>
-        ))}
- 
+                <div className="container">
+                  <div className="row">
+                    <div className="avatar-container col-sm-3 col-md-3">
+                      <img
+                        src={user.avatar}
+                        alt={`Avatar of ${user.name}`}
+                        className="avatar"
+                        width="120"
+                        height="120"
+                      />
+                    </div>
+                    <div className="col-sm-6 col-md-6">
+                      <p>Questions Answered: {user.answered}</p>
+                      <hr></hr>
+                      <p>Questions Created: {user.created}</p>
+                    </div>
+                    <div className="col-sm-3 col-md-3">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">Score</th>
+                          </tr>
+                        </thead>
+                        <tbody >
+                          <tr>
+                            <td>{user.score}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps({users}) {
+function mapStateToProps ({ users }) {
   const items = Object.values(users).map((item) => leaderboardData(item))
+  .sort((a, b) => b.score - a.score)
   return {
     users: items
   }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,13 @@
 export function leaderboardData(user) {
   const { id, name, avatarURL, answers, questions } = user
+  const answered = Object.keys(answers).length
+  const created = Object.keys(questions).length
   return {
     id,
     name,
     avatar: avatarURL,
-    answered: Object.keys(answers).length !== 0 ? answers.length : 0,
-    created: Object.keys(questions).length !== 0 ? questions.length : 0,
-    score: answers.length + questions.length
+    answered: answered !== 0 ? answered : 0,
+    created: created !== 0 ? created : 0,
+    score: answered + created
   }
 }
